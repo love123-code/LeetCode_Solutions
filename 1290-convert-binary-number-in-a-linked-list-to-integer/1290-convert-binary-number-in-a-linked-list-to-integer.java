94795389701fa5cb88917274e9bb19bad1a88df9
love <lovesharma819281@gmail.com>
2026-07-18 23:31:14 +0530

    Time: 4 ms (6.66%), Space: 43.2 MB (6.97%) - LeetHub

--- 1290-convert-binary-number-in-a-linked-list-to-integer/1290-convert-binary-number-in-a-linked-list-to-integer.java
+++ 1290-convert-binary-number-in-a-linked-list-to-integer/1290-convert-binary-number-in-a-linked-list-to-integer.java
@@ -11,15 +11,23 @@
 class Solution {
     public int getDecimalValue(ListNode head) {
 
-        int ans = 0;
+        String s = "";
 
         while (head != null) {
 
-            ans = ans * 2 + head.val;
+            s += head.val;
 
             head = head.next;
         }
 
+        int ans = 0;
+        int num = 1;
+        for(int i=s.length()-1;i>=0;i--){
+            if(s.charAt(i) == '1'){
+                ans += num;
+            }
+            num = num*2;
+        }
         return ans;
     }
 }
